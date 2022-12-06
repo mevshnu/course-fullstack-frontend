ea992fdf71034e090ee72319763706311eab9816
mevshnu <vishnusivan1000@gmail.com>
2022-12-06 17:15:21 +0530

    add,seach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ViewComponent } from './view/view.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ViewComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
